413331b7966819e8d3e51011e481f7b500f2ede9
kimniyom <kimniyomclub@gmail.com>
2019-09-17 14:30:01 +0700

    add query person

--- src/models/person.ts
+++ src/models/person.ts
@@ -9,7 +9,9 @@ export class person {
 
         //แสดงข้อมูลที่รอดำเนินการ
     getPersonCid(db: Knex,cid) {
-      return db('person').where('CID',cid);
+      return db.select('CID', 'NAME', 'LNAME','SEX','TIMESTAMPDIFF(YEAR,BIRTH,NOW()) AS AGE').from('person')
+            .where('CID',cid).limit(1);
+      //return db('person').where('CID',cid).limit(1);
     }
     
 }
